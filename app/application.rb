585701c17dc961170e37fafafa1fc20c422c8367
Alec German <alec_german@yahoo.com>
2018-04-03 21:58:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -20,17 +20,6 @@ class Application
       resp.write "Path Not Found"
     end
 
-    if req.path.match(/cart/)
-      if @@cart.empty?
-        resp.write "Your cart is empty"
-      else
-        @@cart.each{|cart_item| resp.write "#{cart_item}\n"}
-      end
-    elsif req.patch.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    end
-
     resp.finish
   end
 
